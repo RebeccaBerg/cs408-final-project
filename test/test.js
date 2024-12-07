666ca8541566179d28d3657f87a8b15e5734629e
RebeccaBerg <RebeccaBerg@u.boisestate.edu>
2024-12-06 22:21:01 -0700

    Added simple tests for search function

--- test/test.js
+++ test/test.js
@@ -1,11 +1,65 @@
-import {sayHello} from '../js/main.js';
 
-QUnit.module('hello', function() {
+QUnit.module('search', function() {
 
-    QUnit.test('make sure the hello function says hello', function(assert) {
-        var result = sayHello();
-        assert.equal(result, 'hello');
+    QUnit.test('Test Importance Search', function(assert) {
+        var mockTodo = {
+            name: "Dishes",
+            importance: "1",
+            category: "HouseWork",
+            description: "Don't let them pile up!"
+        }
+
+        function search(text) {
+            if (mockTodo.importance.toLowerCase() === text.toLowerCase() || mockTodo.name.toLowerCase() === text.toLowerCase() || mockTodo.category.toLowerCase() === text.toLowerCase() || mockTodo.description.toLowerCase() === text.toLowerCase()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        var result = search("1");
+        assert.equal(result, true);
+    });
+    
+    QUnit.test('Test Importance Search', function(assert) {
+        var mockTodo = {
+            name: "Dishes",
+            importance: "1",
+            category: "HouseWork",
+            description: "Don't let them pile up!"
+        }
+
+        function search(text) {
+            if (mockTodo.importance.toLowerCase() === text.toLowerCase() || mockTodo.name.toLowerCase() === text.toLowerCase() || mockTodo.category.toLowerCase() === text.toLowerCase() || mockTodo.description.toLowerCase() === text.toLowerCase()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        var result = search("4");
+        assert.equal(result, false);
     });
 
+    QUnit.test('Test Importance Search', function(assert) {
+        var mockTodo = {
+            name: "Dishes",
+            importance: "1",
+            category: "HouseWork",
+            description: "Don't let them pile up!"
+        }
 
+        function search(text) {
+            if (mockTodo.importance.toLowerCase() === text.toLowerCase() || mockTodo.name.toLowerCase() === text.toLowerCase() || mockTodo.category.toLowerCase() === text.toLowerCase() || mockTodo.description.toLowerCase() === text.toLowerCase()) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        var result = search("Dishes");
+        assert.equal(result, true);
+    });
 });
+
+
